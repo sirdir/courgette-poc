76c7519abc5dfe81f6490a7bdefd331c8492de41
sirdir <phddir@gmail.com>
2019-03-20 18:28:34 +0200

    update gradle wrapper change to testng

--- src/test/java/com/CR.java
+++ src/test/java/com/CR.java
@@ -1,20 +1,17 @@
 package com;
 
-import courgette.api.CourgetteOptions;
-import courgette.api.CourgetteRunLevel;
-import courgette.api.junit.Courgette;
 import cucumber.api.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Courgette.class)
-@CourgetteOptions(
-        threads = 3,
-        runLevel = CourgetteRunLevel.SCENARIO,
-        reportTargetDir = "build",
-        showTestOutput = true,
-        cucumberOptions = @CucumberOptions(
-                features = "src/test/java/com/features",
-                glue = "com.steps"
-        ))
-public class CR {
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.DataProvider;
+@CucumberOptions(
+        features = {"src/test/java/com/features/"}
+        , glue = {"com.steps"}
+)
+public class CR extends AbstractTestNGCucumberTests {
+        @Override
+        @DataProvider(parallel = true)
+        public Object[][] scenarios() {
+                System.out.println("********");
+                return super.scenarios();
+        }
 }
