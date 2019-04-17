5eee921c8d06df46bc4f9b03820a878b39ee6961
Tymur Kubai <tymur.kubai@exabeam.com>
2019-04-17 15:17:00 +0300

    add custom runner

--- src/test/java/com/steps/CommonStep.java
+++ src/test/java/com/steps/CommonStep.java
@@ -2,16 +2,20 @@ package com.steps;
 
 import cucumber.api.java.en.When;
 
+import java.lang.management.ManagementFactory;
+
 import static java.lang.Thread.currentThread;
 
 public class CommonStep {
 
 
     @When("^run this step$")
-    public void run_this_step() {
+    public void run_this_step() throws InterruptedException {
         System.out.println("-----");
-        System.out.println(currentThread().getId());
+        System.out.println(currentThread().getId() + "   " + ManagementFactory.getRuntimeMXBean().getName());
         System.out.println("-----");
+
+        Thread.sleep(60000);
         assert true;
     }
 
